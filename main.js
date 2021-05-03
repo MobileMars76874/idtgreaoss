417cb6d3964ec3085aed6e2b1726cc4c159fcbdd
MobileMars76874 <72148112+MobileMars76874@users.noreply.github.com>
2021-05-03 17:55:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+D_C = "";
+
+function  preload(){
+D_C = loadImage("dog_cat.jpg");
+}
+
+function setup(){
+canvas = createCanvas(640, 420);
+canvas.center();
+}
+
+function draw(){
+image(D_C, 0, 0, 640, 420);
+fill("#00ffff");
+sroke("#00ffff");
+nofill();
+rect(30, 60, 450, 350);
+}
