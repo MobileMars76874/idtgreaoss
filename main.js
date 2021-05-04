3e6d34659e891a4305a83efc352997dfac3b2584
MobileMars76874 <72148112+MobileMars76874@users.noreply.github.com>
2021-05-04 17:36:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+status = "";
 D_C = "";
 
 function  preload(){
@@ -7,6 +8,15 @@ D_C = loadImage("dog_cat.jpg");
 function setup(){
 canvas = createCanvas(640, 420);
 canvas.center();
+
+OBJ_D = ml5.objectDetector("cocossd", model_Loaded);
+document.getElementById("S").innerHTML = "Status : detecting objects";
+}
+
+function model_Loaded(){
+console.log("Model Loaded!");
+status = "true";
+OBJ_D.detect(D_C, gotresults);
 }
 
 function draw(){
@@ -15,6 +25,15 @@ fill("#00ffff");
 noFill();
 stroke("#00ffff");
 rect(30, 60, 450, 350);
+stroke("#00f763");
 rect(300, 90, 270, 320);
+}
 
+function gotresults(error, results){
+if(error){
+console.log("error");
+}
+else{
+console.log(results);
+}
 }
